1a2e6c8b331651f7360bd808ce06b3436cd4c8e7
MathPerson12321 <mathguy12321@gmail.com>
2024-12-02 22:48:43 -0700

    Day 3

---
+++ AdventCode/Day 3/vals.py
@@ -0,0 +1,26 @@
+import re
+vals = open("values.in","r")
+str = vals.readline().strip()
+product = 1
+# Regular expression to find all instances of mul(x,y)
+pattern = 'mul\(\d+,\d+\)'
+
+# Find all matches in the string
+matches = re.findall(pattern,str)
+for j in matches:
+    i = list(j)
+    i.pop(0)
+    i.pop(0)
+    i.pop(0)
+    i.pop(0)
+    num1 = ""
+    while i[0].isdigit():
+        num1 += i[0]
+        i.pop(0)
+    i.pop(0)
+    num2= ""
+    while i[0].isdigit():
+        num2+= i[0]
+        i.pop(0)
+    product += int(num1)*int(num2)
+print(product)
