431744ad5afb1f32d2848571665c644d18639799
MathPerson12321 <mathguy12321@gmail.com>
2024-12-04 16:14:51 -0700

    Update main.py

--- AdventCode/Day 4/Part 1/main.py
+++ AdventCode/Day 4/Part 1/main.py
@@ -1,46 +1,64 @@
 import re
 vals = open("values.in","r")
-letters = []
+horizontal = []
 pattern1 = "XMAS"
 pattern2 = "SAMX"
 patterns = 0
-for i in range(0,140):
-    letters.append(vals.readline().strip())
-for i in range(0,len(letters)):
-    patternfound = re.findall(pattern1,letters[i])
-    patterns += len(patternfound)
-for i in range(0,len(letters)):
-    patternfound = re.findall(pattern2,letters[i])
-    patterns += len(patternfound)
-for i in range(0,len(list(letters[0]))):
-    for j in range(0,len(letters)-3):
-        lis = list(letters[j])
+for i in range(0,10):
+    horizontal.append(vals.readline().strip())
+
+print(horizontal)
+vertical = []
+for i in range(0,len(horizontal)):
+    string = ""
+    for j in range(0,len(horizontal)):
+        string += list(horizontal[j])[i]
+    vertical.append(string)
+print(vertical)
+
+for i in range(0,len(horizontal)):
+    patternfound = re.findall(pattern1,horizontal[i])
+    patternfound2 = re.findall(pattern2,horizontal[i])
+    patterns += len(patternfound) + len(patternfound2)
+print(patterns)
+
+for i in range(0,len(vertical[0])):
+    for j in range(0,len(vertical[0])-3):
+        lis = vertical[i]
         if(lis[j] == "X" and lis[j+1] == "M" and lis[j+2] == "A" and lis[j+3] == "S"):
             patterns += 1
+            print(lis[j]+lis[j+1]+lis[j+2]+lis[j+3])
+            print(j)
+            print(i)
         elif(lis[j] == "S" and lis[j+1] == "A" and lis[j+2] == "M" and lis[j+3] == "X"):  
             patterns += 1
+            print(lis[j]+lis[j+1]+lis[j+2]+lis[j+3])
+            print(j)
+            print(i)
+print(patterns)
+        
 init = patterns
 patterns = 0
-for i in range(0,len(list(letters[0]))): #Horizontal
-    for j in range(0,len(letters)): #Vertical
-        if(j < len(letters)-3 and i < len(list(letters[0]))-3): #Right, down
-            if(list(letters[j])[i] == "X" and list(letters[j+1])[i+1] == "M" and list(letters[j+2])[i+2] == "A" and list(letters[j+3])[i+3] == "S"):
+for i in range(0,len(list(horizontal[0]))): #Horizontal
+    for j in range(0,len(horizontal)): #Vertical
+        if(j < len(horizontal)-3 and i < len(list(horizontal[0]))-3): #Right, down
+            if(list(horizontal[j])[i] == "X" and list(horizontal[j+1])[i+1] == "M" and list(horizontal[j+2])[i+2] == "A" and list(horizontal[j+3])[i+3] == "S"):
                 patterns += 1
-            elif(list(letters[j])[i] == "S" and list(letters[j+1])[i+1] == "A" and list(letters[j+2])[i+2] == "M" and list(letters[j+3])[i+3] == "X"):
+            elif(list(horizontal[j])[i] == "S" and list(horizontal[j+1])[i+1] == "A" and list(horizontal[j+2])[i+2] == "M" and list(horizontal[j+3])[i+3] == "X"):
                 patterns += 1
         if(j > 2 and i > 2): # Left, up
-            if(list(letters[j])[i] == "X" and list(letters[j-1])[i-1] == "M" and list(letters[j-2])[i-2] == "A" and list(letters[j-3])[i-3] == "S"):
+            if(list(horizontal[j])[i] == "X" and list(horizontal[j-1])[i-1] == "M" and list(horizontal[j-2])[i-2] == "A" and list(horizontal[j-3])[i-3] == "S"):
                 patterns += 1
-            elif(list(letters[j])[i] == "S" and list(letters[j-1])[i-1] == "A" and list(letters[j-2])[i-2] == "M" and list(letters[j-3])[i-3] == "X"):
+            elif(list(horizontal[j])[i] == "S" and list(horizontal[j-1])[i-1] == "A" and list(horizontal[j-2])[i-2] == "M" and list(horizontal[j-3])[i-3] == "X"):
                 patterns += 1
-        if(j < len(letters)-3 and i > 2): # Left, down
-            if(list(letters[j])[i] == "X" and list(letters[j+1])[i-1] == "M" and list(letters[j+2])[i-2] == "A" and list(letters[j+3])[i-3] == "S"):
+        if(j < len(horizontal)-3 and i > 2): # Left, down
+            if(list(horizontal[j])[i] == "X" and list(horizontal[j+1])[i-1] == "M" and list(horizontal[j+2])[i-2] == "A" and list(horizontal[j+3])[i-3] == "S"):
                 patterns += 1
-            elif(list(letters[j])[i] == "S" and list(letters[j+1])[i-1] == "A" and list(letters[j+2])[i-2] == "M" and list(letters[j+3])[i-3] == "X"):
+            elif(list(horizontal[j])[i] == "S" and list(horizontal[j+1])[i-1] == "A" and list(horizontal[j+2])[i-2] == "M" and list(horizontal[j+3])[i-3] == "X"):
                 patterns += 1
-        if(j > 2 and i < len(list(letters[0]))-3): # Right, up
-            if(list(letters[j])[i] == "X" and list(letters[j-1])[i+1] == "M" and list(letters[j-2])[i+2] == "A" and list(letters[j-3])[i+3] == "S"):
+        if(j > 2 and i < len(list(horizontal[0]))-3): # Right, up
+            if(list(horizontal[j])[i] == "X" and list(horizontal[j-1])[i+1] == "M" and list(horizontal[j-2])[i+2] == "A" and list(horizontal[j-3])[i+3] == "S"):
                 patterns += 1
-            elif(list(letters[j])[i] == "S" and list(letters[j-1])[i+1] == "A" and list(letters[j-2])[i+2] == "M" and list(letters[j-3])[i+3] == "X"):
+            elif(list(horizontal[j])[i] == "S" and list(horizontal[j-1])[i+1] == "A" and list(horizontal[j-2])[i+2] == "M" and list(horizontal[j-3])[i+3] == "X"):
                 patterns += 1
-print(str(init+patterns/2))
+print(str(init+patterns))
