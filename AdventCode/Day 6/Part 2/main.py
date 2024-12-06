db03a609832c321710c06fdaa79e9a70acd7cdb2
MathPerson12321 <mathguy12321@gmail.com>
2024-12-06 16:11:40 -0700

    Update main.py

--- AdventCode/Day 6/Part 2/main.py
+++ AdventCode/Day 6/Part 2/main.py
@@ -1,9 +1,9 @@
 import re
 vals = open("values.in","r")
 horizontal,vertical = [],[]
-for i in range(0,130):
+for i in range(0,10):
     horizontal.append(vals.readline().strip())
-obs1 = []
+obs = []
 guard = []
 lengthh = len(horizontal)
 lengthv = len(list(horizontal[0]))
@@ -17,27 +17,21 @@ for i in range(0,lengthh): #Rows
     for j in range(0,lengthv): #Columns
         if(list(horizontal[i])[j] == "#"):
             arr = [i,j]
-            obs1.append(arr)
+            obs.append(arr)
         elif(list(horizontal[i])[j] == "^"):
             guard.append(i)
             guard.append(j)
-positions = [guard]
-posobstruct = []
 count = 0
-dir = 1 #1 = forward, 2 = right, 3 = down, 4 = left (like a compass)
-
 def check(guard):
     return (guard[0] < 0 or guard[0] >= lengthh or guard[1] < 0 or guard[1] >= lengthv)
 
 for i in range(0,lengthh): #Rows
     for j in range(0,lengthv): #Columns
-        obs = []
-        for i in obs1:
-            obs.append(i)
-        if(list(horizontal[i])[j] != "#" and list(horizontal[i])[j] != "^"):
-            arr = [i,j]
-            obs.append(arr)
-        while not check(guard):
+        obs.append([i,j])
+        dir = 1 #1 = forward, 2 = right, 3 = down, 4 = left (like a compass)
+        positions = [guard]
+        brea = False
+        while not check(guard) and not brea:
             if(dir == 1):
                 checkres = False
                 for i in obs:
@@ -49,7 +43,7 @@ for i in range(0,lengthh): #Rows
                 else:
                     dir += 1
                     dir = dir % 4
-            if(dir == 2):
+            elif(dir == 2):
                 checkres = False
                 for i in obs:
                     if(guard[1]+1 == i[1] and guard[0] == i[0]): #Guard ahead
@@ -60,7 +54,7 @@ for i in range(0,lengthh): #Rows
                 else:
                     dir += 1
                     dir = dir % 4
-            if(dir == 3):
+            elif(dir == 3):
                 checkres = False
                 for i in obs:
                     if(guard[0]+1 == i[0] and guard[1] == i[1]): #Guard ahead
@@ -71,7 +65,7 @@ for i in range(0,lengthh): #Rows
                 else:
                     dir += 1
                     dir = dir % 4
-            if(dir == 0):
+            elif(dir == 0):
                 checkres = False
                 for i in obs:
                     if(guard[1]-1 == i[1] and guard[0] == i[0]): #Guard ahead
@@ -82,7 +76,9 @@ for i in range(0,lengthh): #Rows
                 else:
                     dir += 1
                     dir = dir % 4
-            for i in range(len(positions)-4,len(positions)):
-                if positions.count(positions[i]) >= 2:
-                        count += 1
-                
+            for i in positions:
+                if positions.count(i) >= 3:
+                    count += 1
+                    brea = True
+                    break
+print(count)
