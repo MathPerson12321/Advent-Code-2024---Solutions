6f7b2e25d268e25c6b8312a91f75a310cbc2284f
MathPerson12321 <mathguy12321@gmail.com>
2024-12-10 10:24:24 -0700

    >10 min to 45 seconds (D9 P2)

--- AdventCode/Day 9/Part 2/main.py
+++ AdventCode/Day 9/Part 2/main.py
@@ -3,7 +3,7 @@ stri = list(vals.readline().strip())
 arr = []
 idnum = 0
 lengths = []
-#Very long
+dotarray = []
 for i in range(0,len(stri)):
     if(i % 2 == 0):
         for j in range(0,int(stri[i])):
@@ -16,42 +16,39 @@ for i in range(0,len(stri)):
 idnum -= 1
 def check(space,length):
     return int(space) >= int(length)
-for j in range(idnum,-1,-1):
+space = 0
+
+def dotarrayassign():
+    dotarray = []
     space = 0
-    length = lengths[j]
-    lastdot = arr.index(".")
-    lastnodot = arr.index(str(j))+int(length)-1
     for i in range(0,len(arr)):
         if(arr[i] == "."):
             space += 1
         elif space > 0:
-            break
+            dotarray.append([space,i-space])
+            space = 0
+    return dotarray
+    
+for j in range(idnum,-1,-1):
+    print(j) #To keep track of progress
+    space = 0
+    length = lengths[j]
+    while arr[-1] == ".":
+        arr.pop(-1)
+    lastnodot = arr.index(str(j))+int(length)-1
+    dotarray = dotarrayassign()
     indid = arr.index(str(j))
-    if(check(space,length) and lastdot < indid):
-        for i in range(lastdot,lastdot+int(length)):  
-            arr[lastnodot+lastdot-i] = "."
-            arr[i] = str(j)
-    else:
-        while not check(space,length) and lastdot + 1 != len(arr):
-            indid = arr.index(str(j))
-            for i in range(0,len(arr)):
-                if(arr[i] == "." and i > lastdot):
-                    lastdot = i
-                    break
-            a = lastdot
-            for i in range(lastdot,len(arr)):
-                if(arr[i] == "." and i > int(a)):
-                    a = i
-                elif(i > int(a)):
-                    break
-            space = a-int(lastdot)+1
-        if(lastdot + 1 != len(arr) and lastdot < indid):
-            for i in range(lastdot,lastdot+int(length)):  
-                arr[lastnodot+lastdot-i] = "."
-                arr[i] = str(j)
+    for i in dotarray:
+        space = i[0]
+        if(check(space,length) and i[1] < lastnodot):
+            for k in range(i[1],i[1]+int(length)):
+                arr[lastnodot+i[1]-k] = "."
+                arr[k] = str(j)
+            break
 sum = 0
 for i in range(0,len(arr)):
     if(arr[i] != "."):
         sum += i*int(arr[i])   
 print(sum)
+            
             
